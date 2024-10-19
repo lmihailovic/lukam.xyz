6f1fe594457a0ea1122bc2fb82a4627b0cd12580
Lule-M <miha.luka1006@gmail.com>
2024-10-20 01:37:52 +0200

    minor changes

--- mobileCreateBlog.sh
+++ mobileCreateBlog.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+TITLE=$1
+DATE=$2
+CONTENT=$3
+FILENAME=$(echo $1 | sed "s/ /-/g" | tr "[:upper:]" "[:lower:]")
+INDEX_INSERTING_LINENUM=21 
+
 echo '<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +13,16 @@ echo '<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="color-scheme" content="dark light">
     <link rel="stylesheet" href="/css/page-style.css">
-    <title>'$1' | Luka'\''s Webpage</title>
+    <title>'$TITLE' | Luka'\''s Webpage</title>
 </head>
 <body>
     <div class="content">
         <div class="main">
             <span><a href="/blog" class="articleHeading"> &blacktriangleleft; back</a></span>
-            <h1 style="margin: 1em 0 0.2em 0;">'$1'</h1>
-            <p class="articleInfo">'$2'</p>
+            <h1 style="margin: 1em 0 0.2em 0;">'$TITLE'</h1>
+            <p class="articleInfo">'$DATE'</p>
 
-			'$(echo $3 | sed "s/'/\\'/g")'
+			'$(echo $CONTENT | sed "s/'/\\'/g")'
 		</div>
 	</div>
 
@@ -26,4 +32,11 @@ echo '<!DOCTYPE html>
         <a href="/software">software</a><a href="/">home</a><a href="/hobbies">hobbies</a><a href="/about">about</a><br><a href="/Luka_Mihailovic_CV.pdf" download="Luka_Mihailovic_CV.pdf">житије (cv)</a>
     </footer>
 </body>                                                                          
-</html>' > ~/web/lukam.xyz/public_html/blog/$(echo $1 | sed "s/ /-/g" | tr "[:upper:]" "[:lower:]").html
+</html>' > ~/web/lukam.xyz/public_html/blog/$FILENAME.html 
+
+INDEX_HEADLINE_HTML='<div class="article"><h2 class="articleHeading"><a href="/blog/'$FILENAME'.html">'$TITLE'</a></h2><p class="articleInfo">'$DATE'</p></div>'
+
+INDEX_HEADLINE_HTML=$(echo $INDEX_HEADLINE_HTML | sed "s/'/\\'/g")
+
+sed -i "$(echo $INDEX_INSERTING_LINENUM)i\
+	$INDEX_HEADLINE_HTML" ~/web/lukam.xyz/public_html/blog/index.html
